529b1b73e9abc192cfd9cb780c0d84ce36ba8e05
Aline Magalhaes <aoliveiramagalhaes@gmail.com>
2020-08-30 19:56:13 -0400

    add buttons event listeners

--- js/main.js
+++ js/main.js
@@ -1,1 +1,11 @@
-alert("hello")
+window.addEventListener('load', () => {
+
+  document.getElementById("switch-light").addEventListener('click', () => {
+    alert("light")
+  })
+
+  document.getElementById("switch-dark").addEventListener('click', () => {
+    alert("dark")
+  })
+
+});
