30f498e2a02f053231a10f441085102120caf18c
Aline Magalhaes <aoliveiramagalhaes@gmail.com>
2020-08-31 22:36:52 -0400

    Use theme switch toggle
    Based on https://dev.to/ananyaneogi/create-a-dark-light-mode-switch-with-css-variables-34l8

--- js/main.js
+++ js/main.js
@@ -1,32 +1,31 @@
 // add events after loading the page
 window.addEventListener('load', () => {
 
-  // window.scroll({
-  //   top: 0,
-  //   left: 0,
-  //   behavior: 'smooth'
-  // });
+  // Adding the event handlers
+  const toggleSwitch = document.querySelector('.theme-switch input[type="checkbox"]');
 
-  // set theme to light when clicking on the light button
-  document.getElementById("switch-light").addEventListener('click', () => {
-    document.body.setAttribute('data-theme', 'light')
-  })
+  function switchTheme(e) {
+    if (e.target.checked) {
+      document.body.setAttribute('data-theme', 'dark')
+    } else {
+      document.body.setAttribute('data-theme', 'light')
+    }
+  }
 
-  // set theme to dark when clicking on the dark button
-  document.getElementById("switch-dark").addEventListener('click', () => {
-    document.body.setAttribute('data-theme', 'dark')
-  })
+  toggleSwitch.addEventListener('change', switchTheme, false);
 
   // when the page is loaded use the theme configured on the computer
   if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
     document.body.setAttribute('data-theme', 'dark')
+    toggleSwitch.checked = true
   } else {
     document.body.setAttribute('data-theme', 'light')
   }
 
-  // when the theme is changed after loading the page change set it back in the website
+  // when the theme is changed after loading the page set it back in the website
   window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', e => {
     const newColorScheme = e.matches ? "dark" : "light";
+    toggleSwitch.checked = e.matches
 
     document.body.setAttribute('data-theme', newColorScheme)
   });
